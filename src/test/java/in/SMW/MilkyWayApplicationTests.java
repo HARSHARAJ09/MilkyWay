21b0dd9286a1798834023cc937d5e1594b0f0075
HARSHARAJ09 <harsharajbsy@gmail.com>
2026-06-08 16:52:05 +0530

    MilkyWay Project Folder Structure is Created

---
+++ src/test/java/in/SMW/MilkyWayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.SMW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkyWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
